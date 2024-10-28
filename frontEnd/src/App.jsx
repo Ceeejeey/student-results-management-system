9ac1d11c9d6fa1d509a6638d60d441ca827f9de7
Thilina Lakshan <126488588+thilina330@users.noreply.github.com>
2024-10-28 18:19:17 +0530

    update app jsx

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -3,9 +3,15 @@ import StudentDashbord from "./Student/StudentDashBord";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Main11 from "./ui/Main11";
+<<<<<<< Updated upstream
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+=======
+
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+>>>>>>> Stashed changes
 
 import './index.css';
+import Calendar from "./components/Calender";
 
 
 const App = () => {
@@ -20,9 +26,11 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Register />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/admindashbord" element={<AdminDashboard className='bg-blue-500 h-screen' />} />
+          <Route path="/admindashbord" element={<AdminDashboard className='h-screen bg-blue-500' />} />
           <Route path="/studentdashbord" element={<StudentDashbord />} />
           <Route path="/main" element={<Main11/>}/>
+          <Route path="/calender" element={<Calendar/>} />
+          
       
         </Routes>
       
